9398b4fdabf7de85588384042f7dec5b3aebb612
Pavan Marapalli <pavanmarapalli171862@gmail.com>
2023-05-15 13:24:13 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Stopwatch from './components/Stopwatch'
+
+import './App.css'
+
+const App = () => (
+  <div className="container-main">
+    <Stopwatch />
+  </div>
+)
+
+export default App
